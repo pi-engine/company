04f130ed95c5aa26a2561826ff679816ae206050
Hossein Azizabadi Farahani <hossein.azizabadi@gmail.com>
2024-11-10 08:29:24 +0330

    Move security middleware to core module

--- config/module.config.php
+++ config/module.config.php
@@ -8,11 +8,11 @@ use Company\Middleware\PackageMiddleware;
 use Laminas\Mvc\Middleware\PipeSpec;
 use Laminas\Router\Http\Literal;
 use Logger\Middleware\LoggerRequestMiddleware;
+use Pi\Core\Middleware\SecurityMiddleware;
 use User\Middleware\AuthenticationMiddleware;
 use User\Middleware\AuthorizationMiddleware;
 use User\Middleware\InstallerMiddleware;
 use User\Middleware\RequestPreparationMiddleware;
-use User\Middleware\SecurityMiddleware;
 
 return [
     'service_manager' => [
